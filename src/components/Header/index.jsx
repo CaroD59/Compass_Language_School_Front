21daf1ef3fc9c09538adcdf1343cfe162af96b92
CaroD59 <dorchies.c@gmail.com>
2021-07-19 17:07:18 +0200

    Add NavBar

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { ImMenu3 } from 'react-icons/im';
+import { NavLink } from 'react-router-dom';
+import { CgProfile } from 'react-icons/cg';
 import StyledHeader from './styles';
 
 export default function Header() {
+  const [toggleMenu, setToggleMenu] = useState(false);
+  const [largeur, setLargeur] = useState(window.innerWidth);
+  const toggleNavSmallScreen = () => {
+    setToggleMenu(!toggleMenu);
+  };
+
+  useEffect(() => {
+    const changeWidth = () => {
+      setLargeur(window.innerWidth);
+      if (window.innerWidth > 500) {
+        setToggleMenu(false);
+      }
+    };
+    window.addEventListener('resize', changeWidth);
+    return () => {
+      window.removeEventListener('resize', changeWidth);
+    };
+  }, []);
   return (
     <StyledHeader>
       <nav className="NavBar">
@@ -10,6 +31,41 @@ export default function Header() {
             <img src="/img/Icônes/BigLogoCLS.png" alt="logo" />
           </a>
         </div>
+        {(toggleMenu || largeur > 500) && (
+          <div className="TwoContainers">
+            <div className="Container">
+              <div className="Links">
+                <ul>
+                  <li className="Link">
+                    <NavLink to="/">Accueil</NavLink>
+                  </li>
+                  <li className="Link">
+                    <NavLink to="/cours&formules">Cours et Formules</NavLink>
+                  </li>
+                  <li className="Link">
+                    <NavLink to="/recrutement">Recrutement</NavLink>
+                  </li>
+                  <li className="Link">
+                    <NavLink to="/blog">Blog</NavLink>
+                  </li>
+                  <li className="Link">
+                    <NavLink to="/contact">Contact</NavLink>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="Container">
+              <div className="LogoProfile">
+                <NavLink to="/monprofil">
+                  <CgProfile />
+                </NavLink>
+              </div>
+            </div>
+          </div>
+        )}
+        <button onClick={toggleNavSmallScreen} className="btn" type="button">
+          <ImMenu3 />
+        </button>
       </nav>
     </StyledHeader>
   );
--- src/components/Header/styles.jsx
+++ src/components/Header/styles.jsx
@@ -2,12 +2,114 @@ import styled from 'styled-components';
 
 const StyledHeader = styled.div`
   .NavBar {
+    position: fixed;
+    top: 0;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    width: 100vw;
+    height: auto;
+    background-color: #ffff;
+    border-bottom: #b8b8b8 1px solid;
+    box-shadow: 0px 10px 10px rgba(218, 218, 218, 0.25);
     .LogoContainer {
       a {
         img {
           width: 10vw;
           padding-left: 10px;
+          animation: flipInX;
+          animation-duration: 2s;
         }
+      }
+    }
+    .TwoContainers {
+      display: flex;
+      justify-content: space-between;
+      .Container {
+        display: flex;
+        align-items: center;
+        .LogoProfile {
+          animation: flipInX;
+          animation-duration: 2s;
+          a {
+            color: #5e5eaa;
+            font-size: 30px;
+          }
+        }
+        .Links {
+          ul {
+            display: flex;
+            padding: 0;
+            li {
+              display: flex;
+              align-items: center;
+              justify-content: center;
+              margin-right: 110px;
+              list-style-type: none;
+              a {
+                animation: flipInX;
+                animation-duration: 2s;
+                font-family: 'roboto', sans-serif;
+                text-decoration: none;
+                font-size: 13px;
+                color: rgba(70, 70, 70, 1);
+                border-bottom: transparent solid 2px;
+                :hover {
+                }
+                :active {
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+    .btn {
+      display: none;
+    }
+  }
+  @media screen and (max-width: 500px) {
+    .NavBar {
+      flex-direction: column;
+      text-align: center;
+      height: auto;
+      .LogoContainer {
+        a {
+          img {
+            width: 150px;
+            height: 120px;
+          }
+        }
+      }
+      .TwoContainers {
+        .Container {
+          .Links {
+            ul {
+              flex-direction: column;
+              text-align: center;
+              li {
+                height: 50px;
+                width: 100px;
+                li:nth-child(1) {
+                  border-top: 1px solid #565656;
+                  margin-top: 50px;
+                }
+              }
+            }
+          }
+        }
+      }
+      .btn {
+        animation: flipInX;
+        animation-duration: 2s;
+        display: block;
+        position: absolute;
+        right: 10px;
+        top: 10px;
+        color: #333;
+        border: none;
+        font-size: 20px;
+        color: #565656;
       }
     }
   }
